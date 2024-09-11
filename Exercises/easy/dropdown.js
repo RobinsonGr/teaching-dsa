dafc7c2a9bcc109a7ede308338f9115754372b3b
Robinson Gr <77526151+RobinsonGr@users.noreply.github.com>
2024-09-11 00:02:46 -0500

    Create dropdown.js

---
+++ Exercises/easy/dropdown.js
@@ -0,0 +1,34 @@
+const countries = [
+  { name: "India", value: "IN", cities: ["Delhi", "Mumbai"] },
+  { name: "Pak", value: "PK", cities: ["Lahore", "Karachi"] },
+  { name: "Bangladesh", value: "BG", cities: ["Dhaka", "Chittagong"] },
+];
+
+export default function App() {
+  const [selectedCountry, setSelectedCountry] = useState([]);
+
+  const handleCountry = (country) => {
+    setSelectedCountry([]);
+    const selectingCountry = countries.find((element) => element.name);
+    setSelectedCountry(selectingCountry.cities);
+    console.log(selectingCountry);
+  };
+
+  return (
+    <div className="App">
+      <select onChange={(e) => handleCountry(e.target.value)}>
+        {countries.map((country) => (
+          <option>{country.name}</option>
+        ))}
+      </select>
+
+      {selectedCountry.length > 0 && (
+        <select>
+          {selectedCountry.map((city) => (
+            <option>{city}</option>
+          ))}
+        </select>
+      )}
+    </div>
+  );
+}
