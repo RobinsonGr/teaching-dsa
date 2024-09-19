c4618e69119a9cf6362212f892af3b8e7f72da27
Robinson Gr <77526151+RobinsonGr@users.noreply.github.com>
2024-09-19 04:06:43 -0500

    Update and rename i in a row to iInaRow.js

--- Exercises/iInaRow.js
+++ Exercises/iInaRow.js
@@ -18,3 +18,29 @@ function either404(numbers) {
 
     return zeroNextToZero || fourNextToFour;
 }
+
+
+//the easy bizzbuzz
+
+const fizzBuzz = () => {
+  for (let i = 1; i <= 100; i++) {
+    // Check if the number is divisible by both 3 and 5
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("FizzBuzz");
+    } 
+    // Check if the number is divisible by 3
+    else if (i % 3 === 0) {
+      console.log("Fizz");
+    } 
+    // Check if the number is divisible by 5
+    else if (i % 5 === 0) {
+      console.log("Buzz");
+    } 
+    // If none of the conditions match, print the number
+    else {
+      console.log(i);
+    }
+  }
+};
+
+fizzBuzz();
