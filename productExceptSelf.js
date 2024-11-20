76b74cebebf859c45b8d0de273c0eaa8c6ff5351
Robinson Gr <77526151+RobinsonGr@users.noreply.github.com>
2024-11-19 23:59:48 -0500

    Update productExceptSelf.js

--- productExceptSelf.js
+++ productExceptSelf.js
@@ -17,6 +17,6 @@ function productExceptSelf(nums) {
     return answer;
 }
 
-// Example usage:
-console.log(productExceptSelf([1, 2, 3, 4])); // Output: [24, 12, 8, 6]
-console.log(productExceptSelf([-1, 1, 0, -3, 3])); // Output: [0, 0, 9, 0, 0]
+// validationg some test cases
+console.log(productExceptSelf([1, 2, 3, 4])); //  [24, 12, 8, 6]
+console.log(productExceptSelf([-1, 1, 0, -3, 3])); //  [0, 0, 9, 0, 0]
